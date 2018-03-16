1810b675ddff8e97e62f53f1f549a8b42cb26d42
petrisor-diana <36158164+petrisor-diana@users.noreply.github.com>
2018-03-16 10:23:45 +0200

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,9 +1,7 @@
-
-var myDogPic = document.getElementsByClassName('myDog');
 var myDog = document.getElementById('displayCatalog');
-
-
-
+var myFirstPage = document.getElementById('welcomePage');
+var allDogsBreeds = [];
+var myGogInput = document.getElementById('myInput');
 
 var myRequestDog = new XMLHttpRequest();
 myRequestDog.open("GET", "https://dog.ceo/api/breeds/list");
@@ -13,21 +11,84 @@ myRequestDog.addEventListener("load", function onLoad(e) {
     var myResponseAsAJSONBreed = JSON.parse(myResponseAsText);
 
     for (let i = 0; i < myResponseAsAJSONBreed.message.length; i++) {
-        
-        var myRequestPicture = new XMLHttpRequest();
-        myRequestPicture.open("GET", "https://dog.ceo/api/breed/" + myResponseAsAJSONBreed.message[i] + "/images");
-        myRequestPicture.send();
-        myRequestPicture.addEventListener("load", function onLoad(e) {
-            var myResponseAsText = e.target.response;
-            var myResponseAsAJSONPicture = JSON.parse(myResponseAsText);
-  
-            myDog.innerHTML += "<img src=" + myResponseAsAJSONPicture.message[0] + ">";
-            myDog.innerHTML += "<p class='myDog'>" + myResponseAsAJSONBreed.message[i] + "</p>"
-
-        });
+        allDogsBreeds.push(myResponseAsAJSONBreed.message[i]);
     }
 });
 
 
+function getCompanionDogs() {
+    debugger
+    myFirstPage.classList.add('hidden');
+    myDog.classList.remove('hidden');
+    for (let i = 0; i < allDogsBreeds.length / 4; i++) {
+        getPictures(i);
+    }
+}
+console.log(getCompanionDogs)
 
+function getGuardDogs() {
+    debugger
+    myFirstPage.classList.add('hidden');
+    myDog.classList.remove('hidden');
+    for (let i = allDogsBreeds.length / 4; i < allDogsBreeds.length / 2; i++)
+        getPictures(i);
+}
+console.log(getGuardDogs)
+
+function getHuntingDogs() {
+    debugger
+    myFirstPage.classList.add('hidden');
+    myDog.classList.remove('hidden');
+    for (let i = allDogsBreeds.length / 2; i < allDogsBreeds.length - allDogsBreeds.length / 4; i++)
+        getPictures(i);
+}
+
+console.log(getHuntingDogs)
+
+function getWorkingDogs() {
+    debugger
+    myFirstPage.classList.add('hidden');
+    myDog.classList.remove('hidden');
+    for (let i = allDogsBreeds.length - allDogsBreeds.length / 4; i < allDogsBreeds.length; i++)
+        getPictures(i);
+}
+
+console.log(getWorkingDogs)
+
+function showFirstPage() {
+    myDog.classList.add('hidden');
+    myFirstPage.classList.remove('hidden');
+}
+
+
+function getPictures(i) {
+    var myRequestPicture = new XMLHttpRequest();
+    myRequestPicture.open("GET", "https://dog.ceo/api/breed/" + allDogsBreeds[i] + "/images");
+    myRequestPicture.send();
+
+    myRequestPicture.addEventListener("load", function onLoad(e) {
+        var myResponseAsText = e.target.response;
+        var myResponseAsAJSONPicture = JSON.parse(myResponseAsText);
+
+        myDog.innerHTML += '<img class="dogImage" src=' + myResponseAsAJSONPicture.message[0] + ">";
+        myDog.innerHTML += "<p>" + allDogsBreeds[i] + "</p>"
+    });
+}
+
+// function searchDog() {
+
+//     var yourDog = search(myGogInput.value, allDogsBreeds);
+//     getPictures(yourDog);
+
+// }
+
+// function search('affenpinscher', allDogsBreeds) {
+//     for (var i = 0; i < allDogsBreeds.length; i++)
+//         if (list[i].startsWith('affenpinscher')) {
+//             console.log(allDogsBreeds[i]);
+//         }
+//         else
+//             return null;
+
+// }
 
